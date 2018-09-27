b4537eb5cb112b89ecb69b5eb7306dc32451e114
Jorge Ortiz <ortiz171550@unis.edu.gt>
2018-09-26 22:37:43 -0600

    Hoja 6

--- HOJA 6/src/Main.elm
+++ HOJA 6/src/Main.elm
@@ -1,17 +1,30 @@
 module Main exposing (..)
-type Natural
-    = Suc Natural 
-    | Cero 
-    
 
-type alias Model  = 
-    { Sum 
-    x: Int  
-    
-    }
-    
 
-Sum: Int -> Int -> Int 
-Sum x y = 
-    case (x, y) of 
+type Natural = Suc Natural | Cero
 
+enteroANatural : Int -> Natural
+enteroANatural i = if i == 0
+    then Cero
+    else Suc (enteroANatural (i - 1))
+
+
+naturalAEntero : Natural -> Int
+naturalAEntero n = case n of
+    Cero -> 0
+    Suc i -> 1 + naturalAEntero i
+
+
+
+sumar : Natural -> Natural -> Natural
+sumar a1 b1 = case (a1,b1) of
+    (a1_, Cero) -> a1_
+    (Cero, b1_) -> b1_
+    (Suc a1_, b1_) -> Suc (sumar a1_ b1_)
+
+multip : Natural -> Natural -> Natural
+multip a1 b1 =
+    case (a1,b1) of
+        (a1_, Cero) -> Cero
+        (Cero, b1_) -> Cero
+        (Suc a1_, b1_) -> sumar b1_ (multip a1_ b1_)
