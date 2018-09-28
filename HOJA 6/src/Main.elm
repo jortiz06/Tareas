5304370969e1eab8c4412ac18cca41a1bc2306bc
Jorge Ortiz <ortiz171550@unis.edu.gt>
2018-09-28 00:03:31 -0600

    6 y 7

--- HOJA 6/src/Main.elm
+++ HOJA 6/src/Main.elm
@@ -14,7 +14,11 @@ naturalAEntero n = case n of
     Cero -> 0
     Suc i -> 1 + naturalAEntero i
 
-
+resta : Natural -> Natural -> Natural
+resta a1 b1 = case (a1,b1) of
+    (a1_, Cero) -> a1_
+    (Cero, b1_) -> Cero
+    (Suc a1_, Suc b1_) -> resta a1_ b1_
 
 sumar : Natural -> Natural -> Natural
 sumar a1 b1 = case (a1,b1) of
@@ -28,3 +32,11 @@ multip a1 b1 =
         (a1_, Cero) -> Cero
         (Cero, b1_) -> Cero
         (Suc a1_, b1_) -> sumar b1_ (multip a1_ b1_)
+
+division : Natural -> Natural -> (Natural,Natural)
+division a1 b1 = contador (a1)(b1)(Cero)
+contador a1 b1 c1 =
+    if a1== (Suc(Cero))  && b1 == (Suc(Cero)) then ((Suc c1, Cero))
+    else if resta a1 b1 == Cero then (b1, c1)
+    else contador (resta a1 b1) (b1) (Suc c1) 
+    
---
+++ HOJA 7/src/Main.elm
@@ -0,0 +1,9 @@
+module Main exposing (..)
+
+sumar : Int -> Int ->Int
+
+sumar x y = x + y
+
+multiplicar : Int -> Int -> Int
+multiplicar x y = x * y
+
